c2ae8aec76a29f03ec219b4693c5835a4d7212b9
Aranda Solis Ricardo <51523423+ghostdevil24@users.noreply.github.com>
2025-03-01 21:06:28 -0600

    Update PrimerPrograma.c

--- PrimerPrograma.c
+++ PrimerPrograma.c
@@ -6,11 +6,11 @@
 float calcular_tiempo() {
     struct timespec inicio, fin;
     double tiempo_total;
-    float a[1],x[1];
+    float a[1],x;
     a[0]= (float)rand() / (float)RAND_MAX;
     srand(time(NULL));
     clock_gettime(CLOCK_MONOTONIC, &inicio);
-    x[0] = a[0];
+    x = a[0];
     clock_gettime(CLOCK_MONOTONIC, &fin);
     tiempo_total = (fin.tv_sec - inicio.tv_sec) + (fin.tv_nsec - inicio.tv_nsec) / 1E9;
     return (float)tiempo_total;
