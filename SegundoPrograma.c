ce6fad4ae828e42f34c2120cef77e644ee5a33c6
Aranda Solis Ricardo <51523423+ghostdevil24@users.noreply.github.com>
2025-03-01 21:08:22 -0600

    Update SegundoPrograma.c

--- SegundoPrograma.c
+++ SegundoPrograma.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 #define TAMANO 1000
 double calcular_tiempo1() {
-    float a[TAMANO], x[TAMANO];
+    float a[TAMANO], x;
     for (int i = 0; i < TAMANO; i++) {
         a[i] = (float)rand() / (float)RAND_MAX;
     }
     clock_t inicio, fin;
     inicio = clock();
     for (int i = 0; i < TAMANO; i++) {
-        x[i] = a[i];
+        x = a[i];
     }
     fin = clock();
 
